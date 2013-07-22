667b41d5c08c263ddd9dfd40e2d822dfcbcfb648
Lukas Ruebbelke <simpul@gmail.com>
2013-07-22 19:09:18 -0400

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('noteriousApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
