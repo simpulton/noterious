805c8828e1e0ac8a11fed9a3b9079116a1fe87a2
simpulton <simpul@gmail.com>
2013-08-27 13:16:54 -0700

    Edit functionality complete

--- app/scripts/directives/autosize.directive.js
+++ app/scripts/directives/autosize.directive.js
@@ -4,7 +4,12 @@ angular.module('noteriousApp')
     .directive('autosize', function () {
         var linker = function (scope, element, attrs) {
             $(element).autosize();
-        }
+
+            scope.$watch(attrs.ngModel, function(val) {
+                scope.model = val;
+                $(element).trigger('autosize.resize');
+            });
+        };
 
         return {
             restrict: 'A',
