fe44542f2743042b015490b51758547b155dd0d0
Stephen Kuenzli <skuenzli@weblogng.com>
2015-03-21 09:23:21 -0700

    Create a minimal gulp+karma+jasmine testing setup.
    1. instantiate angular app
    2. check that one of the constants is retrievable and correct
    
    use `gulp test` to run once, e.g. in CI.
    
    use `gulp tdd` to run watch for changes and execute tests continuously for TDD workflows.
    
    note:
    * vendoring angularfire because installation from NPM is currently failing.
    * additional dependencies (jQuery, bootstrap, and TweenMax) may need to be added to package.json + karma.conf.js to test all parts of the application.

---
+++ test/app/Noterious.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Noterious: configuration', function () {
+
+  beforeEach(module('noterious'));
+
+  describe('Constant: noterious firebase endpoint', function () {
+
+    var endpointUri;
+
+    beforeEach(inject(function (_ENDPOINT_URI_) {
+      endpointUri = _ENDPOINT_URI_;
+    }));
+
+    it('should be defined', function () {
+      expect(endpointUri).toBeDefined();
+    });
+
+  });
+});
