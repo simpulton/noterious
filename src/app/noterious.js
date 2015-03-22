ba99272c1a3ad3e72a802cac9192d6668904956f
Lukas Ruebbelke <simpul@gmail.com>
2015-03-22 16:30:07 -0700

    Adding in files with correct casing

---
+++ src/app/noterious.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular.module('noterious', [
+  'ui.router',
+  'ngAnimate',
+  'firebase',
+  'noterious.common'
+])
+  .constant('ENDPOINT_URI', 'https://noterious.firebaseio.com/')
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/boards');
+
+    $stateProvider
+      .state('login', {
+        url:'/login',
+        templateUrl: 'app/login/login.tmpl.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+      })
+      .state('boards', {
+        url:'/boards',
+        templateUrl: 'app/boards/boards.tmpl.html',
+        controller: 'BoardsCtrl',
+        controllerAs: 'ctrl',
+        resolve: {
+          'currentUser': ['Auth', function (Auth) {
+            return Auth.$requireAuth();
+          }]
+        }
+      })
+      .state('notes', {
+        url:'/boards/:boardId/notes',
+        templateUrl: 'app/notes/notes.tmpl.html',
+        controller: 'NotesCtrl',
+        controllerAs: 'ctrl',
+        resolve: {
+          'currentUser': ['Auth', function (Auth) {
+            return Auth.$requireAuth();
+          }]
+        }
+      })
+    ;
+  })
+  .run(function ($rootScope, $state) {
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+      event.preventDefault();
+      if (error === 'AUTH_REQUIRED') {
+        $state.go('login');
+      }
+    });
+  })
+;
