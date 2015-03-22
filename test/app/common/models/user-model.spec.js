2c6886f6090fc9f1b7fb8e24f521664bc1ed1771
Stephen Kuenzli <skuenzli@weblogng.com>
2015-03-22 11:44:43 -0700

    Test the UserModel#register

--- test/app/common/models/user-model.spec.js
+++ test/app/common/models/user-model.spec.js
@@ -4,44 +4,82 @@ describe('Service: UserModel', function () {
 
   var UserModel;
   var mockAuth;
+  var scope;
+
+  var createUser = function () {
+    return {
+      email: 'test.user@onehungrymind.com',
+      password: 'super secret!'
+    };
+  };
 
   beforeEach(function () {
+      mockAuth = {};
       module('noterious');
+      module(function ($provide) {
+        $provide.value('Auth', mockAuth);
+      });
 
       //create a simple mock Auth object that returns promises that can be spied-upon
-      mockAuth = {
-        $authWithPassword: function () {
+      inject(function ($q) {
+        mockAuth.$authWithPassword = function () {
           var deferred = $q.defer();
           deferred.resolve("data for successful login");
           return deferred.promise;
-        },
-        $createUser: function () {
+        };
+
+        mockAuth.$createUser = function (user) {
           var deferred = $q.defer();
-          deferred.resolve("data for successful registration");
+          deferred.resolve(
+            {
+              uid: 'uid-42',
+              email: user.email,
+              password: user.password
+            }
+          );
           return deferred.promise;
-        }
-      };
+        };
+      });
 
       spyOn(mockAuth, '$authWithPassword').and.callThrough();
       spyOn(mockAuth, '$createUser').and.callThrough();
 
-      module(function ($provide) {
-        //in angular, services are created via factory functions, return mock
-        var mockAuthFactoryFn = function () { return mockAuth };
-
-        //re-configure angular to provide the mock 'Auth' service
-        $provide.service('Auth', mockAuthFactoryFn);
-      });
     }
   );
 
-
-  beforeEach(inject(function (_UserModel_) {
+  beforeEach(inject(function ($rootScope, _UserModel_) {
+    scope = $rootScope.$new();
     UserModel = _UserModel_;
   }));
 
   it('should be defined', function () {
     expect(UserModel).toBeDefined();
+  });
+
+  describe('register', function () {
+
+    it('should be defined', function () {
+      expect(UserModel.register).toBeDefined();
+    });
+
+    it('should register the provided user and log them in', function () {
+
+      var expectedUser = createUser();
+
+      spyOn(UserModel, 'login');
+
+      UserModel.register(expectedUser);
+
+      scope.$apply(); // promises are resolved/dispatched only on next $digest cycle
+
+      expect(mockAuth.$createUser).toHaveBeenCalledWith({
+        email: expectedUser.email,
+        password: expectedUser.password
+      });
+
+      expect(UserModel.login).toHaveBeenCalledWith(expectedUser.email, expectedUser.password);
+    });
+
   });
 
 });
