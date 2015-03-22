ef73115147a8d43011233fda180630627611fa3e
Stephen Kuenzli <skuenzli@weblogng.com>
2015-03-22 10:21:50 -0700

    Add unit tests verifying behavior of login and invalid form submission.

--- test/app/login/login-controller.spec.js
+++ test/app/login/login-controller.spec.js
@@ -18,6 +18,21 @@ describe('Controller: LoginCtrl', function () {
     };
   };
 
+  var expectRequestForViewTemplate = function(templateQueryPath) {
+    $httpBackend.whenGET(templateQueryPath).respond('{}');
+
+    $httpBackend.flush();
+
+    afterEach(function () {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+  };
+
+  var expectRequestForLoginViewTemplate =  function(){
+    expectRequestForViewTemplate("app/login/login.tmpl.html");
+  };
+
   beforeEach(inject(function ($controller, $state, $q, $rootScope, _$httpBackend_) {
     UserModel = {
       login: function () {
@@ -57,18 +72,9 @@ describe('Controller: LoginCtrl', function () {
     });
 
     it('should register user when valid and registering', function () {
-
-      $httpBackend.whenGET("app/login/login.tmpl.html").respond('{}');
-
-      $httpBackend.flush();
-
-      afterEach(function () {
-        $httpBackend.verifyNoOutstandingExpectation();
-        $httpBackend.verifyNoOutstandingRequest();
-      });
-
       spyOn(state, 'go');
       spyOn(loginCtrl, 'reset').and.callThrough();
+      expectRequestForLoginViewTemplate();
 
       var expectedEmail = 'test.user@onehungrymind.com';
       var expectedPassword = 'super secret!';
@@ -76,7 +82,7 @@ describe('Controller: LoginCtrl', function () {
       loginCtrl.user.email = expectedEmail;
       loginCtrl.user.password = expectedPassword;
 
-      loginCtrl.submit({}, true, true);
+      loginCtrl.submit({ /* ignored */ }, true, true);
 
       expect(UserModel.register).toHaveBeenCalledWith({
         email: expectedEmail,
@@ -88,8 +94,53 @@ describe('Controller: LoginCtrl', function () {
       expect(state.go).toHaveBeenCalledWith('boards');
       expect(loginCtrl.reset).toHaveBeenCalled();
       expect(loginCtrl.user).toEqual(createDefaultUserState());
+      expect(loginCtrl.loading).toBeFalsy();
 
       expect(UserModel.login).not.toHaveBeenCalled();
+    });
+
+    it('should login user when valid and not registering', function () {
+      spyOn(state, 'go');
+      spyOn(loginCtrl, 'reset').and.callThrough();
+      expectRequestForLoginViewTemplate();
+
+      var expectedEmail = 'test.user@onehungrymind.com';
+      var expectedPassword = 'super secret!';
+
+      loginCtrl.user.email = expectedEmail;
+      loginCtrl.user.password = expectedPassword;
+
+      loginCtrl.submit({ /* ignored */ }, true, false);
+
+      expect(UserModel.login).toHaveBeenCalledWith({
+        email: expectedEmail,
+        password: expectedPassword
+      });
+
+      scope.$apply(); // promises are resolved/dispatched only on next $digest cycle
+
+      expect(state.go).toHaveBeenCalledWith('boards');
+      expect(loginCtrl.reset).toHaveBeenCalled();
+      expect(loginCtrl.user).toEqual(createDefaultUserState());
+      expect(loginCtrl.loading).toBeFalsy();
+
+      expect(UserModel.register).not.toHaveBeenCalled();
+    });
+
+    it('should not register nor login user when invalid', function () {
+      spyOn(state, 'go');
+      spyOn(loginCtrl, 'reset');
+      expectRequestForLoginViewTemplate();
+
+      loginCtrl.submit({}, false, true);
+
+      expect(UserModel.register).not.toHaveBeenCalled();
+
+      expect(state.go).not.toHaveBeenCalled();
+      expect(loginCtrl.reset).not.toHaveBeenCalled();
+      expect(UserModel.login).not.toHaveBeenCalled();
+
+      expect(loginCtrl.loading).toBeFalsy();
     });
 
   });
