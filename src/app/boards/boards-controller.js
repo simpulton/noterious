ee84e14bc6da256ea78203ca51f9fe2b0820f3c7
jacobscarter <jacob@ieksolutions.com>
2015-03-26 17:18:04 -0700

    style boards and cards for gridster, styled for text overflow, styled for overflow in gridster mobile mode, enabled resize, changed other gridster options

--- src/app/boards/boards-controller.js
+++ src/app/boards/boards-controller.js
@@ -97,13 +97,13 @@ angular.module('noterious')
 
 
     ctrl.gridsterOpts = {
-      columns: 6, // the width of the grid, in columns
+      columns: 12, // the width of the grid, in columns
       pushing: true, // whether to push other items out of the way on move or resize
       floating: true, // whether to automatically float items up so they stack (you can temporarily disable if you are adding unsorted items with ng-repeat)
       swapping: false, // whether or not to have items of the same size switch places instead of pushing down if they are the same size
       width: 'auto', // can be an integer or 'auto'. 'auto' scales gridster to be the full width of its containing element
       colWidth: 'auto', // can be an integer or 'auto'.  'auto' uses the pixel width of the element divided by 'columns'
-      rowHeight: 'match', // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
+      rowHeight: 80, // can be an integer or 'match'.  Match uses the colWidth, giving you square widgets.
       margins: [10, 10], // the pixel distance between each widget
       outerMargin: true, // whether margins apply to outer edges of the grid
       isMobile: false, // stacks the grid items if true
@@ -112,14 +112,14 @@ angular.module('noterious')
       minColumns: 1, // the minimum columns the grid must have
       minRows: 1, // the minimum height of the grid, in rows
       maxRows: 100,
-      defaultSizeX: 2, // the default width of a gridster item, if not specifed
-      defaultSizeY: 1, // the default height of a gridster item, if not specified
-      minSizeX: 1, // minimum column width of an item
+      defaultSizeX: 3, // the default width of a gridster item, if not specifed
+      defaultSizeY: 2, // the default height of a gridster item, if not specified
+      minSizeX: 3, // minimum column width of an item
       maxSizeX: null, // maximum column width of an item
-      minSizeY: 1, // minumum row height of an item
+      minSizeY: 2, // minumum row height of an item
       maxSizeY: null, // maximum row height of an item
       resizable: {
-        enabled: false
+        enabled: true
       },
       draggable: {
         enabled: true, // whether dragging items is supported
