394b8aacb24178096bbd73249c41791060662c18
Lukas Ruebbelke <simpul@gmail.com>
2015-03-26 23:44:45 -0500

    Pointing to the right view

--- src/app/noterious.js
+++ src/app/noterious.js
@@ -20,7 +20,7 @@ angular.module('noterious', [
       })
       .state('boards', {
         url:'/boards',
-        templateUrl: 'app/boards/boards-grid.tmpl.html',
+        templateUrl: 'app/boards/boards-mdv.tmpl.html',
         controller: 'BoardsCtrl',
         controllerAs: 'ctrl',
         resolve: {
